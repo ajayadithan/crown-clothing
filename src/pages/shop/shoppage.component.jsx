55a89215a6fb999803f6da8219565ef7131ea6af
Ajay <ajayadithan@gmail.com>
2020-05-24 15:44:30 -0600

    Clothing Changes

---
+++ src/pages/shop/shoppage.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import SHOP_DATA from './shop-data'
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {SHOP_DATA.map((shop) => shop )
+                
+                }
+            </div>
+        )
+    }
+}
+
+export default ShopPage
